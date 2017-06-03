b91ae10329d5b5ae8ed86df309c4bb77b01c38c6
0g1 <ogicar000@gmail.com>
2017-06-03 13:39:44 +0200

    Add sync thread

---
+++ src/SyncThread.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ognjen
+ * Date: 6/3/17
+ * Time: 1:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SyncThread implements Runnable {
+
+    public void run() {
+
+    }
+}
