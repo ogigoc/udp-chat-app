614eeb3f6614f70c1bb9708728dfd8039d82167c
0g1 <ogicar000@gmail.com>
2017-06-03 17:16:25 +0200

    ChatGroup all tests passed.

--- src/ChatGroupTest.java
+++ src/ChatGroupTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.net.InetAddress;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -54,6 +55,8 @@ public class ChatGroupTest {
         String nick3 = "ogi";
         address3 = InetAddress.getByName(ip3);
 
+        c.registerPing(address3, nick3);
+
         Assert.assertEquals(c.getAddressToNickname().get(address3), nick3 + "[" + ip3 + "]");
         Assert.assertEquals(c.getAddressToNickname().get(address), nick + "[" + ip + "]");
         Assert.assertEquals(c.getNicknameToAddress().get(nick3 + "[" + ip3 + "]"), address3);
@@ -61,17 +64,53 @@ public class ChatGroupTest {
     }
 
     @Test
-    public void testGetUsername() throws Exception {
-
-    }
-
-    @Test
-    public void testGetAddress() throws Exception {
-
-    }
-
-    @Test
     public void testRemoveDeadClients() throws Exception {
+        ChatGroup c = new ChatGroup();
 
+        String ip = "192.168.10.10";
+        InetAddress address;
+        String nick = "ogi";
+        address = InetAddress.getByName(ip);
+
+        c.registerPing(address, nick);
+
+        InetAddress address2;
+        String nick2 = "david";
+        address2 = InetAddress.getByName("192.168.10.11");
+
+        c.registerPing(address2, nick2);
+
+        Assert.assertEquals(c.getNicknameToAddress().get(nick), address);
+        Assert.assertEquals(c.getAddressToNickname().get(address), nick);
+
+        Assert.assertEquals(c.getNicknameToAddress().get(nick2), address2);
+        Assert.assertEquals(c.getAddressToNickname().get(address2), nick2);
+
+        c.removeDeadClients();
+
+        Assert.assertEquals(c.getNicknameToAddress().get(nick), address);
+        Assert.assertEquals(c.getAddressToNickname().get(address), nick);
+
+        Assert.assertEquals(c.getNicknameToAddress().get(nick2), address2);
+        Assert.assertEquals(c.getAddressToNickname().get(address2), nick2);
+
+        c.getLastPingTime().put(address, new Date(new Date().getTime() - ProtocolConstants.EXPIRATION_TIME - 10));
+
+        /*
+        System.out.println(c.getLastPingTime());
+        System.out.println(c.getAddressToNickname());
+        System.out.println(c.getNicknameToAddress());
+                                    */
+        c.removeDeadClients();
+                                             /*
+        System.out.println(c.getLastPingTime());
+        System.out.println(c.getAddressToNickname());
+        System.out.println(c.getNicknameToAddress());
+                                                                               */
+        Assert.assertEquals(c.getNicknameToAddress().get(nick), null);
+        Assert.assertEquals(c.getAddressToNickname().get(address), null);
+
+        Assert.assertEquals(c.getNicknameToAddress().get(nick2), address2);
+        Assert.assertEquals(c.getAddressToNickname().get(address2), nick2);
     }
 }
