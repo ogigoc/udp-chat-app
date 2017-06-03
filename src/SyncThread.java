dbd73411876bd5dcf74cc0fef7bc14d38ce0204c
0g1 <ogicar000@gmail.com>
2017-06-03 17:53:51 +0200

    Finish SyncThread.

--- src/SyncThread.java
+++ src/SyncThread.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,12 +14,32 @@ import java.net.MulticastSocket;
  */
 public class SyncThread implements Runnable {
     private MulticastSocket socket;
+    private String nickname;
+    private MessageBuilder mb;
 
-    public SyncThread(MulticastSocket socket) {
+    public SyncThread(MulticastSocket socket, InetAddress address, String nickname) {
         this.socket = socket;
+        this.nickname = nickname;
+        this.mb = new MessageBuilder(address, null);
     }
 
     public void run() {
+        while(!Thread.interrupted()) {
+            try {
+                DatagramPacket packet = mb.makePing(nickname);
+                socket.send(packet);
+            } catch (IOException e) {
+                System.err.println("OgiError: Could not send ping packet.");
+                e.printStackTrace();
+            }
 
+            try {
+                Thread.sleep(ProtocolConstants.PING_INTERVAL);
+            } catch (InterruptedException e) {
+                System.err.println("OgiException: SyncThread interupted while speeping.");
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                return;
+            }
+        }
     }
 }
